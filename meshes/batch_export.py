b2e9de27cb6de64436f0e21ab5db1127306eb71b
TilmannR <ti.rub@gmx.de>
2015-12-24 13:51:15 +0100

    Basic rendering.

---
+++ meshes/batch_export.py
@@ -0,0 +1,66 @@
+# exports each selected object into its own file
+
+import bpy
+import os
+from struct import pack
+
+# export to blend file location
+basedir = os.path.dirname(bpy.data.filepath)
+
+if not basedir:
+    raise Exception("Blend file is not saved")
+
+scene = bpy.context.scene
+
+obj_active = scene.objects.active
+selection = bpy.context.selected_objects
+
+bpy.ops.object.select_all(action='DESELECT')
+
+for obj in selection:
+    if obj.type == 'MESH':
+        mesh = obj.data
+        
+        num_verts = len(mesh.vertices)
+        if num_verts == 0:
+            continue
+        
+        name = bpy.path.clean_name(obj.name) + '.mesh'
+        fn = os.path.join(basedir, name)
+        f = open(fn, 'wb')
+        
+        # write number of vertices and triangles
+        num_tris = 0
+        for poly in mesh.polygons:
+            num_tris += len(poly.vertices) - 2
+        f.write(pack('2I', num_verts, num_tris))
+        
+        '''# write triangle data
+        for poly in mesh.polygons:
+            print("Polygon", poly.index, "from loop index", poly.loop_start, "and length", poly.loop_total)
+            for loop_index in poly.loop_indices: # <-- python Range object with the proper indices already set
+                loop = mesh.loops[loop_index] # The loop entry this polygon point refers to
+                vertex = mesh.vertices[loop.vertex_index] # The vertex data that loop entry refers to
+                f.write(pack('I', num_tris))
+                print("\tLoop index", loop.index, "points to vertex index", loop.vertex_index, "at position", vertex.co)
+                for j,uv_layer in enumerate(mesh.uv_layers):
+                    print("\t\tUV Map", j, "has coordinates", uv_layer.data[loop.index].uv, "for this loop index")'''
+        
+        # write vertex data
+        for vert in mesh.vertices:
+            f.write(pack('3f', vert.co.x, vert.co.y, vert.co.z))
+            f.write(pack('3f', vert.normal.x, vert.normal.y, vert.normal.z))
+        
+        # write triangle data
+        for poly in mesh.polygons:
+            for i in range(0, len(poly.vertices) - 2):
+                f.write(pack('3I', poly.vertices[0], poly.vertices[i+1], poly.vertices[i+2]))
+        
+        f.close()
+        print("written:", fn)
+
+
+scene.objects.active = obj_active
+
+for obj in selection:
+    obj.select = True
--- viewer/graphics.py
+++ viewer/graphics.py
@@ -4,16 +4,49 @@ Created on 24.12.2015
 @author: Tilmann
 '''
 
-class MyClass(object):
+import struct
+from OpenGL.raw.GL.VERSION.GL_1_1 import GL_AMBIENT
+
+class Mesh(object):
     '''
     classdocs
     '''
 
-
-    def __init__(self, params):
+    def __init__(self, filename = None):
         '''
         Constructor
         '''
+        self.positions = []
+        self.normals = []
+        self.triangles = []
+        if(filename):
+            self.LoadFromFile(filename)
+    
+    def LoadFromFile(self, filename):
+        f = open(filename, 'rb')
+        num_verts, num_tris = struct.unpack('2I', f.read(8))
+        # read vertices
+        for i in range(num_verts):
+            position = struct.unpack('3f', f.read(12))
+            self.positions.append(position)
+            normal = struct.unpack('3f', f.read(12))
+            self.normals.append(normal)
+        # read triangles
+        for i in range(num_tris):
+            triangle = struct.unpack('3I', f.read(12))
+            self.triangles.append(triangle)
+            
+        f.close()
+        
+    def Draw(self):
+        glBegin(GL_TRIANGLES)
+        glColor3f(1.0, 0.5, 0.0)
+        for triangle in self.triangles:
+            for vertex_index in triangle:
+                glNormal3fv(self.normals[vertex_index])
+                glVertex3fv(self.positions[vertex_index])
+        glEnd()
+        
     
 import pygame
 from pygame.locals import *
@@ -21,53 +54,90 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
-vertices= (
-    (1, -1, -1),
-    (1, 1, -1),
-    (-1, 1, -1),
-    (-1, -1, -1),
-    (1, -1, 1),
-    (1, 1, 1),
-    (-1, -1, 1),
-    (-1, 1, 1)
-    )
-
-edges = (
-    (0,1),
-    (0,3),
-    (0,4),
-    (2,1),
-    (2,3),
-    (2,7),
-    (6,3),
-    (6,4),
-    (6,7),
-    (5,1),
-    (5,4),
-    (5,7)
-    )
-
-def Cube():
-    glBegin(GL_LINES)
-    for edge in edges:
-        for vertex in edge:
-            glVertex3fv(vertices[vertex])
-    glEnd()
-
-def SetCameraMatrix(display):
+def ResizeDisplay(display):
     glViewport(0,0,display[0],display[1])
+
+def SetCameraMatrix(display, angles, distance):
     glLoadIdentity();
     gluPerspective(45, (display[0]/float(display[1])), 0.1, 50.0)
-    glTranslatef(0.0,0.0, -5)
+    glTranslatef(0.0,-1, -distance)
+    glRotatef(angles[1], 1.0, 0.0, 0.0)
+    glRotatef(angles[0], 0.0, 0.0, 1.0)
+
+def DrawGrid():
+    glBegin(GL_LINES)
+    glColor3f(0,0,0)
+    for x in range(-5, 6):
+        glVertex3i(x, -5, 0)
+        glVertex3i(x, 5, 0)
+    for y in range(-5, 6):
+        glVertex3i(-5, y, 0)
+        glVertex3i(5, y, 0)
+    glEnd()
 
 def main():
+    meshes = []
+    meshes.append(Mesh("../meshes/Achse_000.mesh"))
+    meshes.append(Mesh("../meshes/Achse_001.mesh"))
+    meshes.append(Mesh("../meshes/Achse_002.mesh"))
+    meshes.append(Mesh("../meshes/Achse_003.mesh"))
+    meshes.append(Mesh("../meshes/Achse_004.mesh"))
+    meshes.append(Mesh("../meshes/Achse_005.mesh"))
+    meshes.append(Mesh("../meshes/Achse_006.mesh"))
+    
+    translations = [
+        (0, 0, 0.8),
+        (0.4, 0, 0),
+        (1.2, 0, 0),
+        (0.7, 0, 0),
+        (0.3, 0, 0),
+        (0.25, 0, 0),
+        (1.2, -0.3, 0)
+        ]
+    
+    axes = [
+        (0, 0, 1.0),
+        (0, 1.0, 0),
+        (0, 1.0, 0),
+        (1.0, 0, 0),
+        (0, 1.0, 0),
+        (1.0, 0, 0),
+        (0, 1.0, 0)
+        ]
+    
+    limits = [
+        (-5, 365),
+        (-180, 0),
+        (-110, 110),
+        (-180, 180),
+        (-90, 90),
+        (-180, 180),
+        (90, 90)
+        ]
+    
+    angles = [
+              0,0,0,0,0,0,0]
+    angular_velocities = [
+              1,1,1,1,1,1,1]
+    
+    target = (3, 0, 3)
+    
     pygame.init()
     display = (800,600)
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL|RESIZABLE)
 
-    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
-
-    glTranslatef(0.0,0.0, -5)
+    camera_angles = [0,-90]
+    camera_distance = 7;
+    mouse_sensitivity = 0.5
+    
+    glEnable(GL_LIGHT0)
+    glLightfv(GL_LIGHT0, GL_POSITION, (0.0, 1.0, 0.0, 0.0))
+    glLightfv(GL_LIGHT0, GL_AMBIENT, (0.5, 0.5, 0.5, 1.0))
+    glColorMaterial ( GL_FRONT_AND_BACK, GL_AMBIENT ) ;
+    glEnable ( GL_COLOR_MATERIAL ) ;
+    glEnable(GL_DEPTH_TEST)
+    glClearColor(0.75,0.75,0.75,1.0)
+    glPointSize(16.0)
 
     while True:
         for event in pygame.event.get():
@@ -76,15 +146,45 @@ def main():
                 quit()
             elif event.type == pygame.VIDEORESIZE:
                 display = event.dict['size']
-                SetCameraMatrix(display)
+                ResizeDisplay(display)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 5:
+                    camera_distance *= 1.26
+                if event.button == 4:
+                    camera_distance /= 1.26
+        
+        mouse_vel = pygame.mouse.get_rel()
+        if pygame.mouse.get_pressed()[2]:
+            camera_angles[0] += mouse_vel[0] * mouse_sensitivity
+            camera_angles[1] += mouse_vel[1] * mouse_sensitivity
 
-        glRotatef(1, 3, 1, 1)
-        glLineWidth(10.0)
-        glEnable(GL_LINE_SMOOTH)
-        glEnable(GL_BLEND)
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-        Cube()
+        
+        SetCameraMatrix(display, camera_angles, camera_distance)
+        
+        DrawGrid()
+        
+        glEnable(GL_LIGHTING)
+        glPushMatrix()
+        for i in range(len(meshes)):
+            meshes[i].Draw()
+            glTranslatef(translations[i][0], translations[i][1], translations[i][2])
+            glRotatef(angles[i], axes[i][0], axes[i][1], axes[i][2])
+            angles[i] += angular_velocities[i]
+            if angles[i] >= limits[i][1] and angular_velocities[i] > 0 or angles[i] <= limits[i][0] and angular_velocities[i] < 0:
+                angular_velocities[i] *= -1
+        glPopMatrix()
+        glDisable(GL_LIGHTING)
+        
+        glBegin(GL_POINTS)
+        glColor3f(1,1,1)
+        glVertex3fv(target)
+        glEnd()
+        glBegin(GL_LINES)
+        glVertex3f(target[0], target[1], 0)
+        glVertex3fv(target)
+        glEnd()
+        
         pygame.display.flip()
         pygame.time.wait(10)
 
